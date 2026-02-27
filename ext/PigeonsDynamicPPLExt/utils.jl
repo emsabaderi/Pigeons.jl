6eefd2be1950e43e48ba124e327d66e2f1502574
junsongt <junsongt@student.ubc.ca>
2026-02-26 18:24:44 -0800

    fixes in DynamicPPLExt utils

--- ext/PigeonsDynamicPPLExt/utils.jl
+++ ext/PigeonsDynamicPPLExt/utils.jl
@@ -4,10 +4,10 @@ function get_dimension(model::DynamicPPL.Model)
     # get_dimension(DynamicPPL.link!!(DynamicPPL.DynamicTransformation(), vi, model))
 end
 
-# get_dimension(vi::DynamicPPL.VarInfo) = sum(meta -> sum(length, meta.ranges), vi.metadata)
+# get_dimension(vi::DynamicPPL.TypedVarInfo) = sum(meta -> sum(length, meta.ranges), vi.metadata)
 get_dimension(vi::DynamicPPL.VarInfo) = length(DynamicPPL.getindex_internal(vi, :))
 
-# function flatten!(vi::DynamicPPL.VarInfo, dest::Array)
+# function flatten!(vi::DynamicPPL.TypedVarInfo, dest::Array)
 #     i = firstindex(dest)
 #     for meta in vi.metadata
 #         vals = meta.vals
